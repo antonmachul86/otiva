071fb3e241bc89a7c423683eb176e49e2c0042d6
Four44Five55 <Four4Five55@yandex.ru>
2024-04-09 21:54:30 +0300

    TestCommitAndPush

---
+++ src/main/java/ru/skypro/homework/dto/Ads.java
@@ -0,0 +1,11 @@
+package ru.skypro.homework.dto;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Ads {
+    Integer count;
+}
---
+++ src/main/java/ru/skypro/homework/dto/ExtendedAd.java
@@ -0,0 +1,19 @@
+package ru.skypro.homework.dto;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExtendedAd {
+    Integer pk;
+    String authorFirstName;
+    String authorLastName;
+    String description;
+    String email;
+    String image;
+    String phone;
+    Integer price;
+    String title;
+}
